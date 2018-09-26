b51bc1c5021132463b55880cc25037a8525f5a4d
unknown <nivedita1010@gmail.com>
2018-09-26 11:56:44 +0530

    optional- first git commit for my selenium proj

---
+++ src/test/java/calculator/calcy3.java
@@ -0,0 +1,47 @@
+package calculator;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class calcy3 {
+
+	
+	@Test
+	public void sum()
+	{
+		int a =10;
+		int b=20;
+		int sum = a+b;
+		System.out.println("Sum=" + sum );
+		Assert.assertEquals(sum, 30);
+	}
+	
+	@Test
+	public void sub()
+	{
+		int a =10;
+		int b=20;
+		int sub = b-a;
+		System.out.println("sub=" + sub );
+		Assert.assertEquals(sub, 10);
+	}
+	@Test
+	public void mult()
+	{
+		int a =10;
+		int b=20;
+		int mult = a*b;
+		System.out.println("mult=" + mult );
+		Assert.assertEquals(mult, 200);
+	}
+	@Test
+	public void div()
+	{
+		int a =10;
+		int b=20;
+		int div = b/a;
+		System.out.println("div=" + div );
+		Assert.assertEquals(div, 2);
+	}
+	
+}
